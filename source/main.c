db58e81273fcd7577665e88fdec081540c8eb5b5
craft00123 <losc78912@gmail.com>
2022-01-09 20:39:55 +0800

    h

--- source/main.c
+++ source/main.c
@@ -14,13 +14,15 @@ user player;
 
 int card[13] = { 4 }; //撲克牌剩餘數目 0~12
 char num[13] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K' };   //大小 0~12, T = 10
-char ten[] = { 10 };
 
-void showall(user a)	  //0 = banker, 1 = player, 2 = both
+char ans;		      //是否要牌的答案
+
+void showall(user a)	  //顯示所有牌
 {
+	int i;
 	if (a.name == 'b')
 	{
-		int i;
+
 		printf("莊家所有的牌 : ");
 		for (i = 0; i < 5; i++)
 			printf("%c ", num[banker.time[i] - 1]);
@@ -28,7 +30,7 @@ void showall(user a)	  //0 = banker, 1 = player, 2 = both
 	}
 	else if (a.name == 'p')
 	{
-		int i;
+
 		printf("玩家所有的牌   : ");
 		for (i = 0; i < 5; i++)
 			printf("%c ", num[player.time[i] - 1]);
@@ -36,10 +38,52 @@ void showall(user a)	  //0 = banker, 1 = player, 2 = both
 	}
 }
 
+void first2(user a)  //先發2張牌
+{
+	int i;
+	if (a.name == 'b')
+	{
+		for (i = 0; i < 2; i++)
+		{
+			banker.time[i] = rand() % 13 + 1; //1~13
+			if (banker.time[i] > 10)
+				banker.total = banker.total + 10;
+			else
+				banker.total = banker.total + banker.time[i];
+			card[banker.time[i] - 1]--;
+		}
+	}
+	else if (a.name == 'p')
+	{
+		for (i = 0; i < 2; i++)
+		{
+			player.time[i] = rand() % 13 + 1;
+			if (player.time[i] > 10)
+				player.total = player.total + 10;
+			else
+				player.total = player.total + player.time[i];
+			card[player.time[i] - 1]--;
+		}
+	}
+}
+
+void again() //是否再來一局
+{
+	char again;
+	printf("\n再來一局?   n)否  y)是   ");
+	scanf(" %c", &again);
+	if (again == 'y')
+	{
+		system("cls");
+		main();
+	}
+	else if (again == 'n')
+		exit(0);
+}
+
 int main()
 {
-	char ans;		      //是否要牌的答案
-	int i,j;
+	int i, j;
 
 	banker.total = 0;
 	player.total = 0;
@@ -55,36 +99,19 @@ int main()
 	srand(time(NULL));
 	printf("開始遊戲\n發牌\n\n");
 
-	for (i = 0; i < 2; i++)   //一開始雙方掀各發2張牌
-	{
-		banker.time[i] = rand() % 13 + 1; //1~13
-		if (banker.time[i] > 10)
-			banker.total = banker.total + 10;
-		else
-			banker.total = banker.total + banker.time[i];
-		card[banker.time[i] - 1]--;
-	}
-
-	for (i = 0; i < 2; i++)
-	{
-		player.time[i] = rand() % 13 + 1;
-		if (player.time[i] > 10)
-			player.total = player.total + 10;
-		else
-			player.total = player.total + player.time[i];
-		card[player.time[i] - 1]--;
-	}
+	first2(banker);
+	first2(player);
 
 	printf("(T = 10)\n");
-	printf("莊家的牌 : *  %c\n\n", num[banker.time[1]-1]);				//不知道莊家的第一張牌
-	printf("玩家的牌   : %c %c\n\n", num[player.time[0]-1], num[player.time[1]-1]);
+	printf("莊家的牌 : *  %c\n\n", num[banker.time[1] - 1]);				//不知道莊家的第一張牌
+	printf("玩家的牌   : %c %c\n\n", num[player.time[0] - 1], num[player.time[1] - 1]);
 
 	printf("是否要牌?  n)否  y)是  ");
 	scanf(" %c", &ans);
 
-	while(ans != 'n' && ans != 'y')
+	while (ans != 'n' && ans != 'y')
 	{
-		printf("再輸入一次");
+		printf("再輸入一次\n");
 		printf("是否要牌?  n)否  y)是  ");
 		scanf(" %c", &ans);
 	}
@@ -106,29 +133,29 @@ int main()
 		{
 			showall(player);
 			printf("玩家過五關，玩家獲勝\n");
-			return 0;
+			again();
 		}
 		if (player.total > 21)
 		{
 			showall(player);
 			printf("\n莊家獲勝\n");
-			return 0;
+			again();
 		}
 		printf("是否要牌?  n)否  y)是  ");
 		scanf(" %c", &ans);
 	}
 
-	for (i = 0; i < j; i++)
+	for (i = 0; i < j; i++)  //A自動判斷為1或11
 	{
 		if (player.time[i] == 1 && player.total <= 11)
 		{
 			player.total += 10;
 		}
 	}
-		
+
 	printf("輪到莊家\n");
 	j = 2;
-	if(banker.total < 18)
+	if (banker.total < 18)
 	{
 		i = 2;
 		while (banker.total < 17)
@@ -146,13 +173,13 @@ int main()
 			{
 				showall(banker);
 				printf("莊家過五關，莊家獲勝\n");
-				return 0;
+				again();
 			}
 			if (banker.total > 21)
 			{
 				showall(banker);
 				printf("\n玩家獲勝\n");
-				return 0;
+				again();
 			}
 		}
 	}
@@ -165,7 +192,7 @@ int main()
 	}
 
 	printf("\n雙方掀牌\n\n");
-	
+
 	showall(banker);
 	showall(player);
 
@@ -173,11 +200,13 @@ int main()
 		printf("玩家獲勝!\n");
 	else if (banker.total > player.total && banker.total <= 21 && player.total <= 21)
 		printf("莊家獲勝!\n");
-	else if(banker.total == player.total && banker.total <= 21 && player.total <= 21)
+	else if (banker.total == player.total && banker.total <= 21 && player.total <= 21)
 		printf("雙方平手!\n");
 	else
 		printf("no one wins\n");
 	printf("\n");
+
+	again();
 
 	system("pause");
 	return 0;
