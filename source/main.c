adbe96ee4c4ca52a218b7ec01f182a2c79eacd08
craft00123 <losc78912@gmail.com>
2022-01-09 20:48:15 +0800

    fa

--- source/main.c
+++ source/main.c
@@ -72,13 +72,23 @@ void again() //是否再來一局
 	char again;
 	printf("\n再來一局?   n)否  y)是   ");
 	scanf(" %c", &again);
-	if (again == 'y')
+	
+	while (again != 'n' || again != 'y')
 	{
-		system("cls");
-		main();
-	}
-	else if (again == 'n')
-		exit(0);
+		if (again == 'y')
+		{
+			system("cls");
+			main();
+		}
+		else if (again == 'n')
+			exit(0);
+		else
+		{
+			printf("再輸入一次");
+			printf("\n再來一局?   n)否  y)是   ");
+			scanf(" %c", &again);
+		}
+	}	
 }
 
 int main()
